ecd97100b6cfd7a2f3cb459d1024af7bd9e60959
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-05-21 16:58:06 +0200

    Piccole migliorie

--- zotero/search.js
+++ zotero/search.js
@@ -1,65 +1,47 @@
-const uid='14308190'
-const collId='J22KD6XJ'
-const apiKey='nQzLttqa7HtBpPrTmECXG8EN'
-
-const url= `https://api.zotero.org/users/${uid}/tags`
-fetch(url, { headers: {'Zotero-API-Key': apiKey}})
+fetch(`${url}/tags`, { headers: headers})
 .then( (response) => response.json() )
 .then( (jsonList) => {
-  select = document.getElementById('select');
+  document.getElementById('tags').removeAttribute('hidden');
+  document.getElementById('waittags').setAttribute('hidden','');
   jsonList.forEach( t => {
     var opt = document.createElement('option');
     opt.value = t.tag;
     opt.innerHTML = t.tag;
-    select.appendChild(opt);
+    document.getElementById('select').appendChild(opt);
   })
 })
+.catch((error) => { console.log(error) })
 
-select.addEventListener("change",function(e){
+document.getElementById('select').addEventListener('change',function(e){
 //  const outputDiv = document.getElementById("output");
+  document.getElementById('waitoutput').removeAttribute('hidden');
+  document.getElementById('waitoutput').innerHTML = 'Sto cercando nella bibliografia'
+  document.getElementById('output').setAttribute('hidden','');
   var query="";
   Array.from(select.options).filter(x => x.selected).map(x => query += `&tag=${x.value}`);
   console.log(query);//const queryString = window.location.search;
-  const url=`https://api.zotero.org/users/${uid}/collections/${collId}/items?format=json${query}`
-  fetch(url, { headers: {'Zotero-API-Key': apiKey}})
-  .then( (response) => response.json() )
-  .then( (bibList) => {
-    var titleList = '<ul>';
-    bibList.forEach(bib => {
-    var authors = "";
-	bib.data.creators.map ( a => authors += `${a.firstName} ${a.lastName}, ` ) 
-	titleList += `<li> ${authors.slice(0,-1)} ${bib.data.title}`
-	if ( bib.data.url ) {
-	  titleList += ' <a href="' + bib.data.url + '">(link)</a>'
-	}
-  })
-  titleList = titleList + '</ul>'
-  document.getElementById('output').innerHTML = titleList;
-  })
-  .catch((error) => { console.log(error) })
+  if (query !== "") {
+	  fetch(`${url}/items?format=json${query}`, { headers: headers})
+	  .then( (response) => response.json() )
+	  .then( (bibList) => {
+	    var titleList = '<ul>';
+	    bibList.forEach(bib => {
+	    var authors = "";
+		bib.data.creators.map ( a => authors += `${a.firstName} ${a.lastName}, ` ) 
+		titleList += `<li> ${authors.slice(0,-1)} ${bib.data.title}`
+		if ( bib.data.url ) {
+		  titleList += ' <a href="' + bib.data.url + '">(link)</a>'
+		}
+	  })
+	  titleList = titleList + '</ul>'
+	  document.getElementById('output').innerHTML = titleList;
+	  document.getElementById('output').removeAttribute('hidden');
+      document.getElementById('waitoutput').setAttribute('hidden','');
+	  })
+	  .catch((error) => { console.log(error) })
+  } else {
+	document.getElementById('output').innerHTML = "<p><b> Seleziona almeno un tag</b>";
+	document.getElementById('output').removeAttribute('hidden');
+    document.getElementById('waitoutput').setAttribute('hidden','');
+  }
 });
-
-//const url=`https://api.zotero.org/users/${uid}/collections/${collId}/items?format=json&${queryString.slice(1)}`
-//fetch(url, { headers: {'Zotero-API-Key': apiKey}})
-//.then( (response) => response.json() )
-//.then( (bibList) => {
-
-
-//const queryString = window.location.search;
-//const url=`https://api.zotero.org/users/${uid}/collections/${collId}/items?format=json&${queryString.slice(1)}`
-//fetch(url, { headers: {'Zotero-API-Key': apiKey}})
-//.then( (response) => response.json() )
-//.then( (bibList) => {
-  //var titleList = '<ul>';
-  //bibList.forEach(bib => {
-    //var authors = "";
-	//bib.data.creators.map ( a => authors += `${a.firstName} ${a.lastName}, ` ) 
-	//titleList += `<li> ${authors.slice(0,-1)} ${bib.data.title}`
-	//if ( bib.data.url ) {
-	  //titleList += ' <a href="' + bib.data.url + '">(link)</a>'
-	//}
-  //})
-  //titleList = titleList + '</ul>'
-  //document.getElementById('demo').innerHTML = titleList;
-  //})
-//.catch((error) => { console.log(error) })
