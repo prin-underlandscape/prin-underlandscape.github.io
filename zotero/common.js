318b73a2ab4a35450d79915acb726df8e10ba097
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-05-22 11:15:02 +0200

    eliminata private key

--- zotero/common.js
+++ zotero/common.js
@@ -1,13 +1,8 @@
-//const uid='14308190'
-//const collId='J22KD6XJ'
-//const apiKey='nQzLttqa7HtBpPrTmECXG8EN'
-//const url= `https://api.zotero.org/users/${uid}/tags`
-
 const gid = 4553694
-const apiKey = 'PV9fJ2RYqTWpK0CLivM25MPG'
+// const apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXX'
 const url = `https://api.zotero.org/groups/${gid}`
 
 const headers = {
-  'Zotero-API-Key': apiKey,
+//  'Zotero-API-Key': apiKey,
   'Zotero-API-Version': 3
 }
